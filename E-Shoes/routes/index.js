99757b9029c53b8485c63941600de752278b6ed2
Franklin-garcia <franklin__garcia@unitec.edu>
2019-09-22 15:51:41 -0600

    reiniciando hasta paso 5 por errores

--- E-Shoes/routes/index.js
+++ E-Shoes/routes/index.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Product.find(function(err,docs){
-        var productChunks=[];
-        var chunkSize = 3;
-        for(var i = 0; i<docs.length;i += chunkSize){
-            productChunks.push(docs.slice(i, i + chuckSize))
-        }
-        res.render('shop/index', { title: 'E-shoes', products: productChunks}); 
+        res.render('shop/index', { title: 'E-shoes'}); 
     });
-});
 
 module.exports = router;
